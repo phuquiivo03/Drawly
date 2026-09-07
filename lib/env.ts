c274653e93c98ba8c70e6d3ab5917d1ffd64bc2f
zanevo03 <qui@thebinaryholdings.com>
2026-09-07 20:23:06 +0700

    :gear: update env key name

--- lib/env.ts
+++ lib/env.ts
@@ -1,6 +1,6 @@
 export const appEnv = {
   infrastructure: {
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.SUPABASE_PUBLISHABLE_KEY!,
+    supabaseUrl: process.env.SUPABASE_URL!,
   },
 };
