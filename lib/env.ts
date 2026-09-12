6978b02c9df5736a70d3a46194c40c4455477452
zanevo03 <qui@thebinaryholdings.com>
2026-09-12 13:48:44 +0700

    :gear: update appenv

--- lib/env.ts
+++ lib/env.ts
@@ -1,10 +1,21 @@
+const nodeEnv = process.env.NODE_ENV || "development";
 export const appEnv = {
-  infrastructure: {
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    facebook: {
-      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET!,
-      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID!,
-    },
-  },
+  infrastructure:
+    nodeEnv == "development"
+      ? {
+          supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+          supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+          facebook: {
+            clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET!,
+            clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID!,
+          },
+        }
+      : {
+          supabaseKey: process.env.SUPABASE_PUBLISHABLE_KEY!,
+          supabaseUrl: process.env.SUPABASE_URL!,
+          facebook: {
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+            clientId: process.env.FACEBOOK_CLIENT_ID!,
+          },
+        },
 };
