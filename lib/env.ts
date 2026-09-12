31ca5f1bb97c9142dd081fe493a8d0988ec0b92d
zanevo03 <qui@thebinaryholdings.com>
2026-09-12 17:52:47 +0700

    :gear: fix build error

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,4 @@
-const nodeEnv = process.env.NODE_ENV || "development";
+const nodeEnv = process.env.NEXT_PUBLIC_NODE_ENV || "development";
 export const appEnv = {
   infrastructure:
     nodeEnv == "development"
